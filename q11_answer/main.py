215fc812765fc57d2eccc80b31f0f15fba0474b8
Elexzandreia <elexzandreia@csu.fullerton.edu>
2023-05-16 14:32:18 -0700

    fixed errors, now complete

--- q11_answer/main.py
+++ q11_answer/main.py
@@ -3,19 +3,20 @@ from user import *
 def main():
 
     print("AD: Hello! This is a paid advertisment from a nearby restaurant")
-    username = input("Please enter your username")
-    password = input("please enter your password")
+    username = input("Please enter your username: ")
+    password = input("please enter your password: ")
     
     new_user = User(username, password)
-
+    user_choice = '0'
+    
     while(user_choice != '4'):
         print ("option 1: view balance")
         print ("option 2: make deposit")
         print ("option 3: make withdrawal")
         print ("option 4: exit")
-        user_choice = input("Please choose your selection")
+        user_choice = input("Please choose your selection: ")
         if user_choice == '1':
-            new_user.balance()
+            new_user.check_balance()
         elif(user_choice == '2'):
             new_user.deposit()
         elif(user_choice =='3'):
@@ -23,3 +24,5 @@ def main():
         elif(user_choice == '4'):
             print("Goodbye")
             exit
+
+main()
--- q11_answer/user.py
+++ q11_answer/user.py
@@ -1,18 +1,14 @@
-# has a member class/struct User that contains username & password & user activity history & balance information
-# any other info/functions that you believe is necessary.
-# can deposit/withdraw balance after login.
-
 class User():
-    def __init__(self, username, password, history, balance):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
         self.history = {'date':'info'} #dictionary with history info where date (in numbers) is the key and value is description
-        self.balance = balance
+        self.balance = 0.0
 
     def withdrawal(self):
         amount = float(input("Enter withdrawal amount: "))
-        self.balance += amount
+        self.balance -= amount
         print("Your new balance is:", self.balance)
     
     def deposit(self):
@@ -20,5 +16,5 @@ class User():
         self.balance += amount
         print("Your new balance is:", self.balance)
         
-    def balance(self):
+    def check_balance(self):
         print("Your current balance is ", self.balance)
